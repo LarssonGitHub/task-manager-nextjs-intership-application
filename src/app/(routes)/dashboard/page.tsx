65056b3a39ff042fae73af4dfcfd122ac660b18e
LarrsonGitHub <LarssonGitHub>
2024-05-12 14:52:37 +0200

    feat: created temp skeleton for other components

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,13 +1,53 @@
-"use client"
+"use client";
 
-import React from 'react';
-import ProtectedRoute from '@/components/protectedRoutes/ProtectedRoute'; 
-import TaskCardManagementContainer from '@/components/containers/TaskCardManagementContainer';
+import React, { useState, useEffect } from 'react';
+import { fetchTasks } from '@/lib/api/fetchTasks';
+import ProtectedRoute from '@/components/protectedRoutes/ProtectedRoute';
 
 // Your functional component
 function Page() {
+  const [tasks, setTasks] = useState<Tasks | null>(null);
+
+  useEffect(() => {
+    const fetchedTasks = fetchTasks();
+    setTasks(fetchedTasks);
+  }, []);
+
+  if (!tasks) {
+    return <div><p>Loading tasks by user</p></div>;
+  }
+
+  // TODO reuse these components!
+  const listTasksCard = tasks.map((task, index) =>
+    <pre key={index}>{JSON.stringify(task, null, 2)}</pre>
+  );
+
   return (
-<TaskCardManagementContainer/>
+    <main>
+      <section>
+        <h2>Filter by:</h2>
+        <p>filter</p>
+      </section>
+      <h2>Users on your team</h2>
+      <dl>
+        <dt>Admin</dt>
+        <dd>Jonas</dd>
+        <dt>Dev</dt>
+        <dd>Kalle anka</dd>
+      </dl>
+      <section>
+        <h1>Your pending tasks</h1>
+        {listTasksCard}
+      </section>
+      <section>
+        <h2>Your team's pending tasks</h2>
+        {listTasksCard}
+      </section>
+      <section>
+        <h2>Tasks fulfilled</h2>
+        {listTasksCard}
+      </section>
+    </main>
   );
 }
 
