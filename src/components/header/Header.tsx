2f7a92e398f91f0b30454625cfc763abdbd4b400
LarrsonGitHub <LarssonGitHub>
2024-05-12 10:54:59 +0200

    created header and message component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import WelcomeMessage from './WelcomeMessage'
+export default function Header() {
+  console.log("header rendered!")
+  return (
+    <>
+    
+    <WelcomeMessage/>
+    <p>-------------------------</p>
+    </>
+  )
+}
+
---
+++ src/components/header/WelcomeMessage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function WelcomeMessage() {
+  // Do a validation with context
+  return (
+    <div>Welcome guest || user</div>
+  )
+}
