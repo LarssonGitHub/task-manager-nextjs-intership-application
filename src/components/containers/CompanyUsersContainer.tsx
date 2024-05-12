1d028aefcc5c79bd80697f8b80d2ee0c75862e80
LarrsonGitHub <LarssonGitHub>
2024-05-12 10:56:10 +0200

    created companyUsersContainer component

---
+++ src/components/containers/CompanyUsersContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CompanyUsersContainer() {
+  return (
+    <section>
+        <h4>Total users</h4>
+    <ul>
+        <li>User 1 (online-Green-color)</li>
+        <li>User 2 (offline-Red-color)</li>
+    </ul>
+    </section>
+  )
+}
