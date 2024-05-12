07394eccc0e63aeb3e434a24ef8cefcde467e929
LarrsonGitHub <LarssonGitHub>
2024-05-12 10:55:28 +0200

    created taskcard component

---
+++ src/components/card/TaskCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function TaskCard() {
+  return (
+    <div>
+        <div>
+            <span>
+                
+            </span>
+        </div>
+        <h3>name</h3>
+
+    </div>
+  )
+}
