cb3c1763f04ea3918cd3f1ac136e7112c089cace
LarrsonGitHub <LarssonGitHub>
2024-05-12 13:15:55 +0200

    feat: updated header and components for task count display
    the header displays task status, both pending and finished for all logged team members.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import WelcomeMessage from './WelcomeMessage'
-export default function Header() {
-  console.log("header rendered!")
-  return (
-    <>
-    
-    <WelcomeMessage/>
-    <p>-------------------------</p>
-    </>
-  )
-}
+"use client"
 
+import React from 'react';
+import WelcomeMessage from './WelcomeMessage';
+import LogoutButton from './LogoutButton';
+import { useGlobalContext } from '@/context/GlobalContextProvider';
+
+export default function Header() {
+  const { isAuthenticated, setIsAuthenticated } = useGlobalContext();
+  const logoutButton = isAuthenticated ? <LogoutButton /> : null;
+  return (
+    <header>
+      <div>
+        <WelcomeMessage isAuthenticated={isAuthenticated} />
+      </div>
+      <div>
+        {logoutButton}
+      </div>
+    </header>
+  );
+}
--- src/components/header/WelcomeMessage.tsx
+++ src/components/header/WelcomeMessage.tsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { fetchTasks } from '@/lib/api/fetchTasks';
 
-export default function WelcomeMessage() {
-  // Do a validation with context
+interface Task {
+  taskIsComplete: boolean;
+}
+
+interface WelcomeMessageProps {
+  isAuthenticated: boolean;
+}
+
+interface computeTaskStatus {
+  finished: number;
+  pending: number;
+}
+
+function computeTaskStatus(tasks: Task[]): computeTaskStatus {
+  return tasks.reduce((accumulator, task) => {
+    if (task.taskIsComplete) {
+      accumulator.finished++;
+    } else {
+      accumulator.pending++;
+    }
+    return accumulator;
+  }, { finished: 0, pending: 0 });
+}
+
+export default function WelcomeMessage({ isAuthenticated }: WelcomeMessageProps) {
+  const [taskStatus, setTaskStatus] = useState<computeTaskStatus | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isAuthenticated) {
+        const fetchedTasks = fetchTasks();
+        const taskStatus = computeTaskStatus(fetchedTasks);
+        setTaskStatus(taskStatus);
+      }
+    };
+
+    fetchData();
+  }, [isAuthenticated]);
+
+  // If there is time, implement a welcome function
+  // if (!isAuthenticated) {
+  //   return (
+  //     <div>
+  //       <p>Welcome guest!</p>
+  //     </div>
+  //   );
+  // }
+
   return (
-    <div>Welcome guest || user</div>
-  )
+    <div>
+      <p>Welcome to your task manager!!</p>
+      {taskStatus? (
+        <p>Your team has fulfilled {(taskStatus.finished || "error")} tasks, and has {(taskStatus.pending || "error")} pending.</p>
+      ) : (
+        <p>Loading task status...</p>
+      )}
+    </div>
+  );
 }
