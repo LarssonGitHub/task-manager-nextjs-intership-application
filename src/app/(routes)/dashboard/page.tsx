022560566c4cf528e7058b73b9fafbd493b5337d
LarrsonGitHub <LarssonGitHub>
2024-05-13 05:37:55 +0200

    feat: updated task detail page with container components

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,57 +1,89 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { fetchTasks } from '@/lib/api/fetchTasks';
 import ProtectedRoute from '@/components/protectedRoutes/ProtectedRoute';
-import FilterContainer from '@/components/containers/FilterContainer';
-import LoggedUserContainer from '@/components/containers/LoggedUserContainer';
+import FilterContainer from '@/components/containers/dashboard/FilterContainer';
+import LoggedUserContainer from '@/components/containers/dashboard/LoggedUserContainer';
+import { fetchTasks } from '@/lib/api/fetchTasks';
 import { fetchUsers } from '@/lib/api/fetchUsers';
+import { sortTasksAfterFilter } from '@/lib/utils/dataAltering';
+import TaskCardMedium from '@/components/cards/taskCards/TaskCardMedium';
+import { removePendingTasks, removeFinishedTasks } from '@/lib/utils/dataAltering';
 
 // Your functional component
-function Page() {
+export default function Page() {
   const [tasks, setTasks] = useState<Tasks | null>(null);
   const [users, setUsers] = useState<Users | null>(null);
-
-  useEffect(() => {
+  const [filterQuery, setFilterQuery] = useState<FilterQuery>({} as FilterQuery);
+  
+  const getTasks = () => {
     const tasks = fetchTasks();
     setTasks(tasks);
     const users = fetchUsers();
     setUsers(users);
-  }, []);
-
-  if (!tasks) {
-    return <div><p>Loading tasks by user</p></div>;
   }
 
-  // TODO reuse these components!
-  const listTasksCard = tasks.map((task, index) =>
-    <pre key={index}>{JSON.stringify(task, null, 2)}</pre>
-  );
+  const filterTasks = () => {
+    const data = fetchTasks()
+    const sortedTasks = sortTasksAfterFilter(filterQuery, data);
+    setTasks(sortedTasks);
+  }
+
+  useEffect(() => {
+    getTasks()
+  }, []);
+
+  useEffect(() => {
+    filterTasks()
+  }, [filterQuery]);
+
+  function resetOptions() {
+    setFilterQuery({} as FilterQuery);
+    getTasks()
+  }
+
+
+  if (!tasks) {
+    return null;
+  }
+
+  const completedTasks = removePendingTasks(tasks);
+
+  const finishedTasks = completedTasks.map(task => (
+      <TaskCardMedium key={task.id} task={task} />
+  ));
+
+  const notFinishedTasks = removeFinishedTasks(tasks);
+
+  const pendingTasks = notFinishedTasks.map(task => (
+      <TaskCardMedium key={task.id} task={task} />
+  ));
 
   return (
     <main>
       <section>
         <h2>Filter by:</h2>
-        <FilterContainer tasks={tasks} users={users}/>
+        <small>TODO: Values are being reset, but selected option is not, will implement a function.</small>
+        <FilterContainer resetOptions={resetOptions} tasks={tasks} users={users} filterQuery={filterQuery} setFilterQuery={setFilterQuery}/>
       </section>
       <h2>Users on your team</h2>
-      <dl>
         <LoggedUserContainer users={users}/>
-      </dl>
+      {/* 
+      TODO: if time, get the tasks you are working on.
       <section>
         <h1>Your pending tasks</h1>
         {listTasksCard}
+      </section> */}
+      <section>
+        <h3>Your team's pending tasks</h3>
+        {pendingTasks}
       </section>
       <section>
-        <h2>Your team's pending tasks</h2>
-        {listTasksCard}
-      </section>
-      <section>
-        <h2>Tasks fulfilled</h2>
-        {listTasksCard}
+        <h3>Tasks fulfilled</h3>
+        {finishedTasks}
       </section>
     </main>
   );
 }
 
-export default ProtectedRoute(Page);
+// export default ProtectedRoute(Page);
