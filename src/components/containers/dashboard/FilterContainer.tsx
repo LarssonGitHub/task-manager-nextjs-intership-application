5610fd8aa7c5c6c59cc5fdcbaec4861ff7263d1a
LarrsonGitHub <LarssonGitHub>
2024-05-13 05:34:43 +0200

    introduced filter function components for task data

---
+++ src/components/containers/dashboard/FilterContainer.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import FilterOptionsContainer from "./filterOptionsContainer";
+import { getAllFilterOptions } from "@/lib/utils/dataAltering";
+
+interface FilterContainerProps {
+  resetOptions: () => void;
+  tasks: Tasks | null;
+  filterQuery: FilterQuery;
+  users: Users | null;
+  setFilterQuery: React.Dispatch<React.SetStateAction<FilterQuery>>;
+}
+
+export default function FilterContainer({
+  resetOptions,
+  tasks,
+  users,
+  filterQuery,
+  setFilterQuery,
+}: FilterContainerProps) {
+  const [filterOptions, setFilterOptions] = useState<FilterOptions | null>(null);
+
+  useEffect(() => {
+    if (!tasks ||!users) return;
+    const options = getAllFilterOptions(tasks, users);
+    setFilterOptions(options);
+  }, []);
+
+  return (
+      <FilterOptionsContainer
+      resetOptions={resetOptions}
+      filterOptions={filterOptions}
+      filterQuery={filterQuery}
+      setFilterQuery={setFilterQuery}
+    />
+  );
+}
---
+++ src/components/containers/dashboard/filterOptionsContainer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Select from '../../inputs/Select';
+
+interface FilterContainerProps {
+  resetOptions: () => void;
+  filterOptions: React.SetStateAction<FilterOptions | null>;
+  filterQuery: FilterQuery;
+  setFilterQuery: React.Dispatch<React.SetStateAction<FilterQuery>>;
+}
+
+export default function FilterOptionsContainer({resetOptions, filterOptions, filterQuery, setFilterQuery }: FilterContainerProps) {
+  
+  if (!filterOptions) return null;
+
+  function updateFilterState(value: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>, name: string) {
+    setFilterQuery(prev => ({
+     ...prev,
+      [name]: value.target.value, 
+    }));
+  }
+
+  const selects = Object.entries(filterOptions).map(([key, value]) => (
+    <Select
+      key={key}
+      name={key}
+      values={value}
+      handleChange={updateFilterState} 
+    />
+  ));
+
+  return (
+    <div>
+      {selects}
+      {/* TODO check if GenericInputElement component is of use here */}
+      <input type="date" id="startDate" name="startDate" onChange={(e)=> updateFilterState(e, "startDate")} />
+      <button onClick={resetOptions}>Reset</button>
+      <button onClick={() => console.log(filterQuery)}>state</button>
+    </div>
+  );
+}
