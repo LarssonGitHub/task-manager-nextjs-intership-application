67830c046f38015b65f30cf359ecff16986b12dc
LarrsonGitHub <LarssonGitHub>
2024-05-13 06:53:07 +0200

    refactor: cleaned up component

--- src/components/cards/userCards/LoggedUserCards.tsx
+++ src/components/cards/userCards/LoggedUserCards.tsx
@@ -7,16 +7,19 @@ interface LoggedUserCardsProps {
 export default function LoggedUserCards({ categorizedUsers }: LoggedUserCardsProps) {
   if (!categorizedUsers) return null;
 
+  // Correctly define loggedUserCard as an array of JSX elements
+  const loggedUserCard = Object.keys(categorizedUsers || {}).map((category, index) => (
+    <dl key={index}>
+      <dt>{category}:</dt>
+      {categorizedUsers[category].map((user, index) => (
+        <dd key={index}>{user.userName}</dd>
+      ))}
+    </dl>
+  ));
+
   return (
     <>
-      {Object.keys(categorizedUsers || {}).map((category, index) => (
-        <dl key={index}>
-          <dt>{category}:</dt>
-          {categorizedUsers[category].map((user, index) => (
-            <dd key={index}>{user.userName}</dd>
-          ))}
-        </dl>
-      ))}
+      {loggedUserCard}
     </>
   );
 }
