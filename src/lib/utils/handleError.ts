0775905aa6cc67aedd637771325f6fe319d3ac99
LarrsonGitHub <LarssonGitHub>
2024-05-14 02:43:26 +0200

    feat: implemented very basic error handler for authentication and login

---
+++ src/lib/utils/handleError.ts
@@ -0,0 +1,19 @@
+import { destroyToken } from "@/auth/authTokenManager";
+
+export function handleError(error: unknown) {
+    console.error(error);
+    if (error && typeof error === 'object' && error.hasOwnProperty('message')) {
+      // @ts-ignore
+      window.alert(`Error: ${error.message}`);
+      return;
+    } 
+    window.alert('An unknown error occurred, check logs.');
+  }
+
+  export function handleTokenVerifyingError(error: unknown) {
+  destroyToken()
+  console.log(error)
+  alert("Token and authentication couldn't be verified, check error logs for more info. Redirecting and destroying session....")
+  window.location.href = "/login"
+  }
+
