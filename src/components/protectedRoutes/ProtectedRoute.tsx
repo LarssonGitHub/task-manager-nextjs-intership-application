0d771b1f62dd42653ab2d367084d4a34b9771097
LarrsonGitHub <LarssonGitHub>
2024-05-14 02:34:03 +0200

    fix: resolve authentication issue in ProtectedRoute component
    the bug was that it ran twice, looping authentication conditions and nullifying any and all logic. It now works as intended, protects routes, and deletes cookies and sessions if authentication failure occurs.

--- src/components/protectedRoutes/ProtectedRoute.tsx
+++ src/components/protectedRoutes/ProtectedRoute.tsx
@@ -1,93 +1,43 @@
-// // TODO further test this protectedRoute, or fully rework everything in it!
-// // Since it always does a check when user changes routes, it slows down the load time, giving a bad user experience
-// // Furthermore, it is most likely far from good for SEO.
-
-
-// // TODO Test to add a condition which only checks the useGlobalContext.
-// // Since the backend requests are checked too, they will not be able to access th DB, even if they change global context in frontend
-
-// "use client";
-
-// import React, { useState, useEffect } from "react";
-// import { useGlobalContext } from "@/context/GlobalContextProvider";
-// import { redirect } from "next/navigation";
-// import { validateToken } from "@/auth/authTokenManager";
-
-// type ProtectedRouteProps = React.FC
-
-// function ProtectedRoute(WrappedComponent: ProtectedRouteProps) {
-//   return function (props: any) {
-//     const { isAuthenticated, setIsAuthenticated } = useGlobalContext();
-//     const [isAuthenticating, setIsAuthenticating] = useState(true); 
-
-//     useEffect(() => {
-//       const checkAuthentication = async () => {
-//         setIsAuthenticating(true)
-//         const isValidated = await validateToken();
-//         console.log("protectedRouteran", isValidated)
-//         setIsAuthenticated(false);
-//         setIsAuthenticating(false)
-//       };
-//       checkAuthentication();
-//     }, []);
-
-//     if (isAuthenticating) return null;
-//     if (!isAuthenticated &&!isAuthenticating) {
-//       return redirect("/login");
-//     }
-
-//     return <WrappedComponent {...props} />;
-//   };
-// }
-
-// export default ProtectedRoute;
-
-// // https://www.youtube.com/watch?v=woaUOS8fqBw
-// // https://github.com/pagecow/nextjs-13-context-api
-// // https://www.freecodecamp.org/news/secure-routes-in-next-js/
-// // https://www.youtube.com/watch?v=DJvM2lSPn6w
-
-
 "use client";
 
 import React, { useState, useEffect } from "react";
 import { useGlobalContext } from "@/context/GlobalContextProvider";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { validateToken } from "@/auth/authTokenManager";
+import { destroyToken } from "@/auth/authTokenManager";
 
-type ProtectedRouteProps = React.ComponentType<any>;
 
- function ProtectedRoute(WrappedComponent: ProtectedRouteProps) {
-  const DisplayedComponent = (props: any) => {
+export default function ProtectedRoute(Page: any) {
+  return function CheckAuthentication(props: any) {
     const { isAuthenticated, setIsAuthenticated } = useGlobalContext();
-        const [isAuthenticating, setIsAuthenticating] = useState(true); 
+    const [shouldRender, setShouldRender] = useState<true | null>(null);
+    const router = useRouter();
 
-    
-        const checkAuthentication = async () => {
-          console.log(isAuthenticated)
-          setIsAuthenticating(true)
-          const isValidated = await validateToken();
-          setIsAuthenticated(isValidated);
-          setIsAuthenticating(false)
-          console.log(isAuthenticated)
-        };
-        
-        useEffect(() => {
-          checkAuthentication();
-        }, []);
-    
-        if (isAuthenticating) return null;
-        if (!isAuthenticated &&!isAuthenticating) {
-          return redirect("/login");
-        }
-    return <WrappedComponent {...props} />;
+    const destroyUser = () => {
+      destroyToken()
+      router.push("/login");
+    }
+
+    const checkAuthentication = async () => {
+      if (isAuthenticated) return;
+      // TODO if time, try libraries or other ways to keep the global state persistent when manually changing url. 
+      // Storing and using a token like this to validate if user is isAuthenticated or not on frontend might not be the best way
+      // Especially when it is a front only The token is encrypted, but as it is only on frontend, it 
+      const isValidated = await validateToken();
+      if (!isValidated) {
+      destroyUser()
+      return;
+      }
+      setIsAuthenticated(isValidated);
+      setShouldRender(true);
+    };
+
+    useEffect(() => {
+      checkAuthentication();
+    }, []);
+
+    if (!shouldRender) return null;
+
+    return <Page {...props} />;
   };
-
-  // TODO: Hack, check if it this can be rewritten to make it better
-  DisplayedComponent.displayName = WrappedComponent.displayName || WrappedComponent.name || 'Component';
-
-  return DisplayedComponent;
 }
-
-export default ProtectedRoute;
-
