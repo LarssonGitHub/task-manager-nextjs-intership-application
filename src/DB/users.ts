8b7a8af84558333bef8411a8d7ebd39a1bbb2f7a
LarrsonGitHub <LarssonGitHub>
2024-05-14 02:51:27 +0200

    fix: corrected bad name value in mock user-db

--- src/DB/users.ts
+++ src/DB/users.ts
@@ -1,6 +1,6 @@
 export const users = [
   { id: 1, userCategory: "Admin", userName: "Admin 1", email: "admin1@email.com", password: "passw0rd" },
-  { id: 2, userCategory: "tester", userName: "tester 1", email: "tester1@email.com", password: "passw0rd" },
+  { id: 2, userCategory: "Tester", userName: "Tester 1", email: "tester1@email.com", password: "passw0rd" },
   { id: 3, userCategory: "Developer", userName: "Developer 1", email: "developer1@email.com", password: "passw0rd" },
   { id: 4, userCategory: "Developer", userName: "Developer 2", email: "designer1@email.com", password: "passw0rd" },
   { id: 5, userCategory: "Manager", userName: "Manager 1", email: "tester2@email.com", password: "passw0rd" }
